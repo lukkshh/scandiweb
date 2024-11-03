c5cb6040edb9c04ac6135c53c1aea19ab1bed654
lukkshh <xlukkass@gmail.com>
2024-11-03 19:56:57 +0400

    refactor and implement image slide with arrows

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,8 +1,8 @@
 import Card from "../components/Card";
-
 import React from "react";
 
-import { ApolloConsumer } from "@apollo/client";
+import withGraphQl from "../utils/withGraphQL";
+
 import { gql } from "@apollo/client";
 
 const GET_PRODUCTS = gql`
@@ -29,28 +29,9 @@ const GET_PRODUCTS = gql`
 `;
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-    };
-  }
-
-  fetchProducts = (client) => {
-    client
-      .query({
-        query: GET_PRODUCTS,
-      })
-      .then((result) => {
-        this.setState({ products: result.data.products });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   render() {
     const currentPath = window.location.pathname;
+    const { data } = this.props;
 
     const paths = [
       { path: "/", label: "WOMEN" },
@@ -62,31 +43,21 @@ class Home extends React.Component {
       paths.find((item) => item.path === currentPath)?.label || "/";
 
     return (
-      <ApolloConsumer>
-        {(client) => {
-          this.fetchProducts(client);
+      <section className="w-full">
+        <div className="w-full h-40 flex items-center">
+          <p className="text-[42px] ml-24 text-[#1D1F22]">{currentLabel}</p>
+        </div>
 
-          return (
-            <section className="w-full">
-              <div className="w-full h-40 flex items-center">
-                <p className="text-[42px] ml-24 text-[#1D1F22]">
-                  {currentLabel}
-                </p>
-              </div>
-
-              <div className="w-full flex justify-center items-center">
-                <div className="grid grid-cols-3 gap-16">
-                  {this.state.products.map((product, index) => (
-                    <Card key={index} data={product} />
-                  ))}
-                </div>
-              </div>
-            </section>
-          );
-        }}
-      </ApolloConsumer>
+        <div className="w-full flex justify-center items-center">
+          <div className="grid grid-cols-3 gap-16">
+            {data.products.map((product, index) => (
+              <Card key={index} data={product} />
+            ))}
+          </div>
+        </div>
+      </section>
     );
   }
 }
 
-export default Home;
+export default withGraphQl(Home, GET_PRODUCTS);
--- frontend/src/pages/PDP.jsx
+++ frontend/src/pages/PDP.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import withRouter from "../utils/withRouter";
 import parse from "html-react-parser";
 import addToCart from "../utils/addToCart";
-import { ApolloConsumer, gql } from "@apollo/client";
+import { gql } from "@apollo/client";
+import withGraphQl from "../utils/withGraphQL";
 
 const GET_PRODUCT = gql`
   query GetProduct($id: String!) {
@@ -38,63 +39,54 @@ class PDP extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: {},
-    };
-    this.item = {
-      id: this.props.params.id,
-      img: "",
-      name: "",
-      quantity: 1,
-      size: "",
-      color: "",
-      price: "",
-      currencySymbol: "",
+      currentImg: props.data.product.gallery[0],
+      selectedSize: "",
+      selectedColor: "",
     };
   }
 
   handleSizeChange = (value) => {
-    this.item.size = value;
+    this.setState({ selectedSize: value });
   };
 
   handleColorChange = (value) => {
-    this.item.color = value;
-  };
-
-  fetchProduct = (client) => {
-    client
-      .query({
-        query: GET_PRODUCT,
-        variables: { id: this.props.params.id },
-      })
-      .then((result) => {
-        this.setState({ product: result.data.product });
-      })
-      .catch((error) => console.error(error));
+    this.setState({ selectedColor: value });
   };
 
   handleAddToCart = () => {
-    this.item.price = this.state.product.prices[0].amount;
-    this.item.img = this.state.product.gallery[0];
-    this.item.name = this.state.product.name;
-    this.item.currencySymbol = this.state.product.prices[0].currency.symbol;
-    addToCart(this.item);
+    const { data } = this.props;
+    const { selectedSize, selectedColor } = this.state;
+
+    const item = {
+      id: this.props.params.id,
+      img: data.product.gallery[0],
+      name: data.product.name,
+      quantity: 1,
+      size: selectedSize,
+      color: selectedColor,
+      price: data.product.prices[0].amount,
+      currencySymbol: data.product.prices[0].currency.symbol,
+    };
+
+    addToCart(item);
   };
 
   renderSizeOptions() {
     const sizes = ["XS", "S", "M", "L"];
+
     return sizes.map((size) => (
       <div className="relative" key={size}>
         <input
           className="cursor-pointer appearance-none h-[45px] w-[63px] border-2 border-[#1D1F22] rounded-sm checked:bg-[#1D1F22] focus:outline-none"
           type="radio"
-          name="size"
+          name="Size"
           value={size}
           id={`size${size}`}
           onChange={() => this.handleSizeChange(size)}
         />
         <label
           className={`${
-            this.item.size === size ? "text-white" : "text-black"
+            this.state.selectedSize === size ? "text-white" : "text-black"
           } cursor-pointer font-roboto absolute left-1/2 translate-x-[-50%] top-3`}
           htmlFor={`size${size}`}
         >
@@ -119,67 +111,105 @@ class PDP extends React.Component {
     ));
   }
 
+  handleImgChange = (img) => {
+    this.setState({ currentImg: img });
+  };
+
+  handlePrevImage = () => {
+    const { data } = this.props;
+    const { currentImg } = this.state;
+
+    let imgCount = data.product.gallery.indexOf(currentImg);
+
+    imgCount =
+      (imgCount - 1 + data.product.gallery.length) %
+      data.product.gallery.length;
+
+    this.handleImgChange(data.product.gallery[imgCount]);
+  };
+
+  handleNextImage = () => {
+    const { data } = this.props;
+    const { currentImg } = this.state;
+
+    let imgCount = data.product.gallery.indexOf(currentImg);
+
+    imgCount = (imgCount + 1) % data.product.gallery.length;
+
+    this.handleImgChange(data.product.gallery[imgCount]);
+  };
+
   render() {
+    const { data } = this.props;
+    const { currentImg, selectedSize, selectedColor } = this.state;
+
     return (
-      <ApolloConsumer>
-        {(client) => {
-          this.fetchProduct(client);
-          const { product } = this.state;
-          return (
-            <section className="flex m-[140px]">
-              <div className="flex space-x-12" data-testid="product-gallery">
-                <div className="space-y-8 max-h-[478px] overflow-y-auto no-scrollbar ">
-                  {product.gallery?.map((img, key) => (
-                    <img
-                      key={key}
-                      className="object-contain w-20 h-20"
-                      src={img}
-                      alt=""
-                    />
-                  ))}
-                </div>
-                <div>
-                  <img
-                    className="w-[575px] h-[478px] object-contain"
-                    src={product.gallery?.[0]}
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="ml-[200px]">
-                <p className="font-semibold text-3xl">{product.name}</p>
-                <p className="uppercase mt-4 text-lg font-bold">Size:</p>
-                <div className="flex space-x-2">{this.renderSizeOptions()}</div>
-                <p className="uppercase mt-4 text-lg font-bold">Color:</p>
-                <div className="space-x-2">{this.renderColorOptions()}</div>
-                <p className="uppercase mt-4 text-lg font-bold">Price:</p>
-                <p className="uppercase mt-4 text-2xl font-bold ">
-                  {product.prices?.[0].currency.symbol}
-                  {product.prices?.[0].amount}
-                </p>
-                <button
-                  data-testid="add-to-cart"
-                  disabled={
-                    !product.inStock || !this.item.size || !this.item.color
-                  }
-                  onClick={this.handleAddToCart}
-                  className="bg-[#5ECE7B] font-semibold mt-6 disabled:bg-[#99dbab] w-[292px] h-[43px] text-white uppercase"
-                >
-                  Add To Cart
-                </button>
-                <div
-                  className="pr-[150px] mt-6  max-w-[900px]"
-                  data-testid="product-description"
-                >
-                  {parse(product.description || "")}
-                </div>
-              </div>
-            </section>
-          );
-        }}
-      </ApolloConsumer>
+      <section className="flex m-[140px]">
+        <div className="flex space-x-12" data-testid="product-gallery">
+          <div className="space-y-8 max-h-[478px] overflow-y-auto no-scrollbar">
+            {data.product.gallery.map((img, key) => (
+              <img
+                onClick={() => this.handleImgChange(img)}
+                key={key}
+                className="cursor-pointer object-contain w-20 h-20"
+                src={img}
+                alt={`gallery-img-${key}`}
+              />
+            ))}
+          </div>
+          <div className="relative">
+            <div className="absolute top-1/2 translate-y-[-50%] w-full flex justify-between">
+              <button
+                onClick={() => this.handlePrevImage()}
+                className="w-[31.7px] h-[31.7px] flex justify-center items-center bg-[#000000BA]"
+              >
+                <img src="/CaretLeft.svg" alt="" />
+              </button>
+              <button
+                onClick={() => {
+                  this.handleNextImage();
+                }}
+                className="w-[31.7px] h-[31.7px] flex justify-center items-center bg-[#000000BA]"
+              >
+                <img src="/CaretRight.svg" alt="" />
+              </button>
+            </div>
+            <img
+              className="w-[575px] h-[478px] object-contain"
+              src={currentImg}
+              alt="main-img"
+            />
+          </div>
+        </div>
+        <div className="ml-[200px]">
+          <p className="font-semibold text-3xl">{data.product.name}</p>
+          <p className="uppercase mt-4 text-lg font-bold">Size:</p>
+          <div className="flex space-x-2">{this.renderSizeOptions()}</div>
+          <p className="uppercase mt-4 text-lg font-bold">Color:</p>
+          <div className="space-x-2">{this.renderColorOptions()}</div>
+          <p className="uppercase mt-4 text-lg font-bold">Price:</p>
+          <p className="uppercase mt-4 text-2xl font-bold">
+            {data.product.prices[0].currency.symbol}
+            {data.product.prices[0].amount}
+          </p>
+          <button
+            data-testid="add-to-cart"
+            disabled={!data.product.inStock || !selectedSize || !selectedColor}
+            onClick={this.handleAddToCart}
+            className="bg-[#5ECE7B] font-semibold mt-6 disabled:bg-[#99dbab] w-[292px] h-[43px] text-white uppercase"
+          >
+            Add To Cart
+          </button>
+          <div
+            className="pr-[150px] mt-6 max-w-[900px]"
+            data-testid="product-description"
+          >
+            {parse(data.product.description || "")}
+          </div>
+        </div>
+      </section>
     );
   }
 }
 
-export default withRouter(PDP);
+export default withRouter(withGraphQl(PDP, GET_PRODUCT));
