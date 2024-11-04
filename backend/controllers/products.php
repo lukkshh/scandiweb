cf6d9ad16a38c4d2a852df9f8c0e2d966024d4e7
lukkshh <xlukkass@gmail.com>
2024-11-04 21:11:58 +0400

    implement get product by category

--- backend/controllers/products.php
+++ backend/controllers/products.php
@@ -22,6 +22,22 @@ class Products {
         return $products;
 
     }
+    
+    public function getProductsByCategory($category) {
+        $statement = $this->db->prepare("SELECT * FROM products WHERE category = :category");
+        $statement->bindParam(':category', $category, PDO::PARAM_STR);
+        $statement->execute();
+        $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        // Decode the gallery, attributes and prices field to make sure it's an array
+        foreach ($products as &$product) {
+            $product['gallery'] = json_decode($product['gallery'], true);
+            $product['attributes'] = json_decode($product['attributes'], true);
+            $product['prices'] = json_decode($product['prices'], true);
+        }   
+
+        return $products;
+    }
 
     public function getCategories() {
         $statement = $this->db->prepare("SELECT * FROM categories");
