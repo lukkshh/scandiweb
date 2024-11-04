40abecd5668c3a19432d138d05d3cb31e9dd5f06
lukkshh <xlukkass@gmail.com>
2024-11-05 00:08:56 +0400

    fixes

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import addToCart from "../utils/addToCart";
 
 class Card extends React.Component {
@@ -7,6 +6,29 @@ class Card extends React.Component {
     const { data } = this.props;
 
     const handleAddToCart = () => {
+      if (!Array.isArray(data.attributes) || data.attributes.length === 0) {
+        addToCart({
+          id: data.id,
+          quantity: 1,
+          price: data.prices[0].amount,
+          img: data.gallery[0],
+          name: data.name,
+          currencySymbol: data.prices[0].currency.symbol,
+        });
+        return;
+      }
+
+      const findAttribute = (attributes, attributeId) =>
+        Array.isArray(attributes)
+          ? attributes.find((attr) => attr.id === attributeId)?.items || []
+          : [];
+
+      const selectedSize = findAttribute(data.attributes, "Size")[0]
+        ?.displayValue;
+      const selectedColor = findAttribute(data.attributes, "Color")[0]?.value;
+      const selectedCapacity = findAttribute(data.attributes, "Capacity")[0]
+        ?.displayValue;
+
       const newItem = {
         id: data.id,
         quantity: 1,
@@ -14,22 +36,13 @@ class Card extends React.Component {
         img: data.gallery[0],
         name: data.name,
         currencySymbol: data.prices[0].currency.symbol,
+        ...(selectedSize && { size: selectedSize }),
+        ...(selectedColor && { color: selectedColor }),
+        ...(selectedCapacity && { capacity: selectedCapacity }),
+        availableSizes: findAttribute(data.attributes, "Size"),
+        availableColors: findAttribute(data.attributes, "Color"),
+        availableCapacities: findAttribute(data.attributes, "Capacity"),
       };
-
-      if (data.attributes.length === 0) {
-        addToCart(newItem);
-        return;
-      }
-
-      data.attributes.forEach((attribute) => {
-        if (attribute.id === "Size") {
-          newItem.size = attribute.items[0].displayValue;
-        } else if (attribute.id === "Color") {
-          newItem.color = attribute.items[0].displayValue;
-        } else {
-          newItem[attribute.id] = attribute.items[0].displayValue;
-        }
-      });
 
       addToCart(newItem);
     };
@@ -37,27 +50,24 @@ class Card extends React.Component {
     return (
       <div
         data-testid={`product-${data.id}`}
-        className={` ${
+        className={`${
           data.inStock ? "" : "out-of-stock"
         } w-[386px] h-[444px] group relative flex flex-col justify-center items-center customShadow`}
       >
         <button
           disabled={!data.inStock}
-          onClick={() => {
-            handleAddToCart();
-          }}
-          className={` ${
+          onClick={handleAddToCart}
+          className={`${
             data.inStock ? "" : "group-hover:hidden"
-          } z-10 hidden group-hover:flex  w-[52px] h-[52px] top-[304px] left-[287px] absolute bg-[#5ECE7B] rounded-full justify-center items-center`}
+          } z-10 hidden group-hover:flex w-[52px] h-[52px] top-[304px] left-[287px] absolute bg-[#5ECE7B] rounded-full justify-center items-center`}
         >
           <img src="white-cart.svg" alt="Cart" />
         </button>
         <a href={`/${data.category}/${data.id}`}>
           <img
-            className="w-[354px] h-[330px] object-contain
-          "
+            className="w-[354px] h-[330px] object-contain"
             src={data.gallery[0]}
-            alt=""
+            alt={data.name}
           />
         </a>
         <div className="w-[354px] h-[58px] mt-[24px]">
